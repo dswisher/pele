4e274668cb0dd23fe460e5218565f6cf0f7a9ba8
Doug Swisher <big.swish@gmail.com>
2017-03-17 07:30:39 -0500

    Larger window; 1.6 -> 1.8

--- desktop/src/xyz/dswisher/pele/desktop/DesktopLauncher.java
+++ desktop/src/xyz/dswisher/pele/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import xyz.dswisher.pele.PeleGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pele";
+		config.width = 800;
+		config.height = 600;
+
 		new LwjglApplication(new PeleGame(), config);
 	}
 }
