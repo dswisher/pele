d3f76176f7c0d11746b1a1b745c6c2c571c6f6d2
Doug Swisher <big.swish@gmail.com>
2017-03-16 21:06:50 -0500

    Initial commit

---
+++ desktop/src/xyz/dswisher/pele/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.dswisher.pele.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.dswisher.pele.PeleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PeleGame(), config);
+	}
+}
