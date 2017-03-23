52f757006e341151e870b37a1368211bc6c9d68a
Doug Swisher <big.swish@gmail.com>
2017-03-22 22:09:28 -0500

    More menu tinkering

--- core/src/xyz/dswisher/pele/MainMenuScreen.java
+++ core/src/xyz/dswisher/pele/MainMenuScreen.java
@@ -2,13 +2,18 @@ package xyz.dswisher.pele;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -19,6 +24,7 @@ public class MainMenuScreen implements Screen {
     private Skin skin;
     private Stage stage;
     private Game game;
+    private OrthographicCamera camera;
 
     public MainMenuScreen(Game game) {
         this.game = game;
@@ -28,6 +34,9 @@ public class MainMenuScreen implements Screen {
     private void create() {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 400);
 
         // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
         // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
@@ -44,7 +53,7 @@ public class MainMenuScreen implements Screen {
         BitmapFont bfont = new BitmapFont();
         // TODO - .scale() seems to have gone away; does it matter? Should we use FreeTypeFontGenerator?
         // bfont.scale(1);
-        skin.add("default",bfont);
+        skin.add("default", bfont);
 
         // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
@@ -58,22 +67,45 @@ public class MainMenuScreen implements Screen {
         skin.add("default", textButtonStyle);
 
         // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
-        final TextButton textButton=new TextButton("PLAY",textButtonStyle);
-        textButton.setPosition(200, 200);
-        stage.addActor(textButton);
-        stage.addActor(textButton);
-        stage.addActor(textButton);
+        final TextButton playButton = new TextButton("PLAY", textButtonStyle);
+        playButton.setPosition(200, 200);
+        stage.addActor(playButton);
+
+        final TextButton quitButton = new TextButton("QUIT", textButtonStyle);
+        quitButton.setPosition(400, 200);
+        stage.addActor(quitButton);
 
         // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
         // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
         // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
         // revert the checked state.
-        textButton.addListener(new ChangeListener() {
+        playButton.addListener(new ChangeListener() {
             public void changed (ChangeListener.ChangeEvent event, Actor actor) {
-                //System.out.println("Clicked! Is checked: " + button.isChecked());
-                textButton.setText("Starting new game");
-                game.setScreen( new MainGameScreen());
+                game.setScreen(new MainGameScreen());
+            }
+        });
 
+        quitButton.addListener(new ChangeListener() {
+            public void changed (ChangeListener.ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        // Respond to keys, too!
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean keyUp(InputEvent event, int keycode) {
+                switch (keycode) {
+                    case Input.Keys.ENTER:
+                        game.setScreen(new MainGameScreen());
+                        break;
+
+                    case Input.Keys.Q:
+                    case Input.Keys.ESCAPE:
+                        Gdx.app.exit();
+                        break;
+                }
+                return false;
             }
         });
     }
